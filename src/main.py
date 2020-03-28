86a38122c170add5a25843fde4fad1a3fceb2ff5
Suriya Narayanan Lakshmanan <suriyanarayanan987@gmail.com>
2020-03-28 12:32:54 -0700

    BO using loss as the objective
    Signed-off-by: Suriya Narayanan Lakshmanan <suriyanarayanan987@gmail.com>

--- src/main.py
+++ src/main.py
@@ -15,14 +15,15 @@ from logger import Logger
 from datasets.dataset_factory import get_dataset
 from trains.train_factory import train_factory
 
+from ax.plot.contour import plot_contour
+from ax.plot.trace import optimization_trace_single_method
+from ax.service.managed_loop import optimize
+from ax.utils.notebook.plotting import render, init_notebook_plotting
 
-def main(opt):
-  torch.manual_seed(opt.seed)
-  torch.backends.cudnn.benchmark = not opt.not_cuda_benchmark and not opt.test
-  Dataset = get_dataset(opt.dataset, opt.task)
-  opt = opts().update_dataset_info_and_set_heads(opt, Dataset)
-  print(opt)
+import numpy as np
 
+
+def main(opt, train_loader, val_loader):
   logger = Logger(opt)
 
   os.environ['CUDA_VISIBLE_DEVICES'] = opt.gpus_str
@@ -39,29 +40,6 @@ def main(opt):
   Trainer = train_factory[opt.task]
   trainer = Trainer(opt, model, optimizer)
   trainer.set_device(opt.gpus, opt.chunk_sizes, opt.device)
-
-  print('Setting up data...')
-  val_loader = torch.utils.data.DataLoader(
-      Dataset(opt, 'val'), 
-      batch_size=1, 
-      shuffle=False,
-      num_workers=1,
-      pin_memory=True
-  )
-
-  if opt.test:
-    _, preds = trainer.val(0, val_loader)
-    val_loader.dataset.run_eval(preds, opt.save_dir)
-    return
-
-  train_loader = torch.utils.data.DataLoader(
-      Dataset(opt, 'train'), 
-      batch_size=opt.batch_size, 
-      shuffle=True,
-      num_workers=opt.num_workers,
-      pin_memory=True,
-      drop_last=True
-  )
 
   print('Starting training...')
   best = 1e10
@@ -95,8 +73,70 @@ def main(opt):
       print('Drop LR to', lr)
       for param_group in optimizer.param_groups:
           param_group['lr'] = lr
+
+  # Final validation for BO
+  # with torch.no_grad():
+  with torch.no_grad():
+    log_dict_val, preds = trainer.val(opt.num_epochs+1, val_loader)
+
   logger.close()
+  return log_dict_val['loss']
+
+def train_evaluate(parameterization):
+  opt = opts().parse()
+  opt.lr = parameterization["lr"]
+
+  torch.manual_seed(opt.seed)
+  torch.backends.cudnn.benchmark = not opt.not_cuda_benchmark and not opt.test
+  Dataset = get_dataset(opt.dataset, opt.task)
+  opt = opts().update_dataset_info_and_set_heads(opt, Dataset)
+  print(opt)
+
+  print('Setting up data...')
+  val_loader = torch.utils.data.DataLoader(
+      Dataset(opt, 'val'), 
+      batch_size=1, 
+      shuffle=False,
+      num_workers=1,
+      pin_memory=True
+  )
+
+  if opt.test:
+    _, preds = trainer.val(0, val_loader)
+    val_loader.dataset.run_eval(preds, opt.save_dir)
+    return
+
+  train_loader = torch.utils.data.DataLoader(
+      Dataset(opt, 'train'), 
+      batch_size=opt.batch_size, 
+      shuffle=True,
+      num_workers=opt.num_workers,
+      pin_memory=True,
+      drop_last=True
+  )
+
+  return main(opt, train_loader, val_loader)
 
 if __name__ == '__main__':
-  opt = opts().parse()
-  main(opt)
+  best_parameters, values, experiment, model = optimize(
+    parameters = [
+      {"name":"lr", "type": "range", "bounds": [1e-6, 1e-3], "log_scale": True},
+    ],
+    evaluation_function = train_evaluate,
+    objective_name='loss',
+    minimize=True,
+    total_trials=50
+  )
+  
+  print("Best params: ", best_parameters)
+  mean, covariances = values
+  print("Mean loss: ", mean)
+  print("Loss Covariance: ", covariances)
+
+  best_objectives = np.array([[trial.objective_mean*100 for trial in experiment.trials.values()]])
+  best_objective_plot = optimization_trace_single_method(
+      y=np.maximum.accumulate(best_objectives, axis=1),
+      title="Model performance vs. # of iterations",
+      ylabel="Classification Accuracy, %",
+  )
+  render(best_objective_plot)
